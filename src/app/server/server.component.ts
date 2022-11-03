b7d513bb4151ad6f76db6180a051a53b200bb7e4
Purushuttam Kumar <55408162+purushuttam@users.noreply.github.com>
2022-11-03 10:47:53 +0530

    o
    l

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,11 +1,22 @@
-
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-selector: 'app-server',
-template: './server.component.html'
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
 })
+export class ServerComponent implements OnInit {
 
-export class ServerComponent{
+  serverId = 10;
+  serverStatus = 'offline';
 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getServerStatus(){
+    return this.serverStatus;
+  }
 }
